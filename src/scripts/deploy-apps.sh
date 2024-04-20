f0b25cadeebe348e72df56c500b006d8a30eeecd
GBANE Assouman <gbane.assouman@gmail.com>
2024-04-20 20:08:59 +0000

    add create staging ec2

--- src/scripts/deploy-apps.sh
+++ src/scripts/deploy-apps.sh
@@ -2,7 +2,5 @@
 export instance_ip=$(curl https://checkip.amazonaws.com)
 #cd app-dir/
 version=$(awk 'NR==3 {print $2}' releases.txt) docker-compose up -d --build
-pwd
-ls -ltrh
 sleep 10
 docker ps -a
